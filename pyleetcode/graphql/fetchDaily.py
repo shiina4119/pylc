ac79c1039e9b056046fa2d7bddca7289180774c3
shiina <152995083+shiina4119@users.noreply.github.com>
2024-05-24 14:42:11 +0530

    fetchDaily.py: fetch titleSlug of daily problem

---
+++ pyleetcode/graphql/fetchDaily.py
@@ -0,0 +1,19 @@
+import requests
+from globals import gqlUrl, generateHeaders
+
+
+def fetchDailyQuestion() -> dict:
+    headers = generateHeaders()
+    query = """
+    query {
+      activeDailyCodingChallengeQuestion {
+        question {
+          titleSlug
+        }
+      }
+    }
+    """
+    # TODO: handle error
+    response = requests.post(url=gqlUrl, json={"query": query}, headers=headers)
+    data = response.json()
+    return data["data"]["activeDailyCodingChallengeQuestion"]["question"]
