4d91969fab6b5a487c23ccddc73443f0f9050afc
shiina <152995083+shiina4119@users.noreply.github.com>
2024-05-24 14:44:33 +0530

    fetchQuestionContent.py: fetch question body by supplying titleSlug

---
+++ pyleetcode/graphql/fetchQuestionContent.py
@@ -0,0 +1,27 @@
+import requests
+from globals import gqlUrl, generateHeaders
+
+
+def fetchQuestionContent(titleSlug: str) -> dict:
+    # TODO: fetch question using question_id
+    headers = generateHeaders()
+    query = """
+    query ($titleSlug: String!) {
+      question(titleSlug: $titleSlug) {
+        questionId
+        content
+        mysqlSchemas
+        exampleTestcaseList
+        topicTags {
+          name
+        }
+      }
+    }
+    """
+    variables = {"titleSlug": titleSlug}
+    # TODO: handle error
+    response = requests.post(
+        url=gqlUrl, json={"query": query, "variables": variables}, headers=headers
+    )
+    data = response.json()
+    return data["data"]["question"]
