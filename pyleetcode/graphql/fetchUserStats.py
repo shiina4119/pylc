876b948f723687641dfb6b2e64694fba898999a3
shiina <152995083+shiina4119@users.noreply.github.com>
2024-05-24 14:44:07 +0530

    fetchUserStats.py: fetch problems solved by user

---
+++ pyleetcode/graphql/fetchUserStats.py
@@ -0,0 +1,33 @@
+import requests
+from globals import gqlUrl, generateHeaders
+from fetchUserData import fetchUserData
+
+
+def fetchUserStats() -> dict:
+    headers = generateHeaders()
+    user = fetchUserData()
+    query = """
+    query ($username: String!) {
+      allQuestionsCount {
+        difficulty
+        count
+      }
+      matchedUser(username: $username) {
+        submitStats {
+          acSubmissionNum {
+            difficulty
+            count
+          }
+        }
+      }
+    }
+    """
+    # TODO: handle error
+    variables = {"username": user["username"]}
+    response = requests.post(
+        url=gqlUrl,
+        json={"query": query, "variables": variables},
+        headers=headers,
+    )
+    data = response.json()
+    return data
