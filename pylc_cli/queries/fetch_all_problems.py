d01c487a0a05fe6e6e9166c586628d0cd5370e2f
shiina <152995083+shiina4119@users.noreply.github.com>
2024-06-02 18:46:17 +0530

    fetch_all_problems.py: fetch metadata and content for all problems

---
+++ pylc_cli/queries/fetch_all_problems.py
@@ -0,0 +1,101 @@
+import requests
+from . import GRAPHQL_URL, generate_headers
+
+
+def fetch_all_problems_count() -> int:
+    headers = generate_headers()
+    query = """
+    query {
+      allQuestionsCount {
+        count
+      }
+    }
+    """
+    response = requests.post(url=GRAPHQL_URL, json={"query": query}, headers=headers)
+    if response.status_code != 200:
+        # TODO: handle 403 errors nicely
+        raise ConnectionError
+
+    json = response.json()
+    count = json["data"]["allQuestionsCount"][0]["count"]
+    return count
+
+
+def fetch_all_problems_metadata():
+    headers = generate_headers()
+    count = fetch_all_problems_count()
+    query = """
+    query ($categorySlug: String
+    $limit: Int
+    $skip: Int
+    $filters: QuestionListFilterInput) {
+      questionList(
+        categorySlug: $categorySlug
+        limit: $limit
+        skip: $skip
+        filters: $filters
+      ) {
+        totalNum
+        data {
+          questionId,
+          questionFrontendId,
+          title,
+          difficulty,
+          isPaidOnly,       
+        }
+      }
+    }
+    """
+    variables = {"categorySlug": "", "skip": 0, "limit": count, "filters": {}}
+
+    response = requests.post(
+        url=GRAPHQL_URL, json={"query": query, "variables": variables}, headers=headers
+    )
+
+    if response.status_code != 200:
+        # TODO: handle 403 errors nicely
+        print(response.status_code)
+        raise ConnectionError
+
+    json = response.json()
+    return json["data"]["questionList"]["data"]
+
+
+def fetch_all_problems_content():
+    headers = generate_headers()
+    count = fetch_all_problems_count()
+    query = """
+    query ($categorySlug: String
+    $limit: Int
+    $skip: Int
+    $filters: QuestionListFilterInput) {
+      questionList(
+        categorySlug: $categorySlug
+        limit: $limit
+        skip: $skip
+        filters: $filters
+      ) {
+        totalNum
+        data {
+          questionFrontendId,
+          content,         
+          topicTags {
+            name
+          }
+        }
+      }
+    }
+    """
+    variables = {"categorySlug": "", "skip": 0, "limit": count, "filters": {}}
+
+    response = requests.post(
+        url=GRAPHQL_URL, json={"query": query, "variables": variables}, headers=headers
+    )
+
+    if response.status_code != 200:
+        # TODO: handle 403 errors nicely
+        print(response.status_code)
+        raise ConnectionError
+
+    json = response.json()
+    return json["data"]["questionList"]["data"]
