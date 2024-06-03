040467bb8afbecacca771f2149dfc5e5429a7167
shiina <152995083+shiina4119@users.noreply.github.com>
2024-06-03 16:57:01 +0530

    Refactor run_solution() to work with database

--- pylc_cli/run_solution.py
+++ pylc_cli/run_solution.py
@@ -1,8 +1,6 @@
-import ast
 from pathlib import Path
 from rich.padding import Padding
-from . import BASE_DIR, console, EXT_MAP
-from .queries.fetch_problem import fetch_problem
+from . import BASE_DIR, console, EXT_MAP, con
 from .queries.judge import send_judge
 from .queries.status import get_status
 
@@ -18,12 +16,16 @@ def stringify_code(file_path: str) -> str:
     return "".join(lines)
 
 
-def run(title_slug: str, lang: str, test: bool):
-    question = fetch_problem(title_slug=title_slug)
-    id = question["questionFrontendId"]
+def run(id: int, lang: str, test: bool):
+    res = con.execute(
+        f"SELECT frontend_id, id, title_slug FROM metadata WHERE frontend_id = {id}"
+    )
+    data = res.fetchone()
+    title_slug = data["title_slug"]
+    frontend_id = data["frontend_id"]
     ext = EXT_MAP[lang]
 
-    file_path = f"{BASE_DIR}/code/{id}.{title_slug}.{lang}.{ext}"
+    file_path = f"{BASE_DIR}/code/{frontend_id}.{title_slug}.{lang}.{ext}"
     if not Path(file_path).is_file():
         raise FileNotFoundError
 
@@ -31,7 +33,11 @@ def run(title_slug: str, lang: str, test: bool):
 
     with console.status(status="Sending code to server...", spinner="monkey"):
         run_id = send_judge(
-            title_slug=title_slug, lang=lang, typed_code=typed_code, test=test
+            title_slug=title_slug,
+            id=data["id"],
+            lang=lang,
+            typed_code=typed_code,
+            test=test
         )
         status = get_status(title_slug=title_slug, id=run_id, test=test)
 
@@ -43,21 +49,19 @@ def run(title_slug: str, lang: str, test: bool):
                 console.print(Padding("Wrong Answer", (1, 2), style="bold red"))
 
             console.print(Padding(
-                f"Expected answer: {status["expected_code_answer"]}", 
-                (0, 2)
-            ))
-            console.print(Padding(
-                f"Your answer: {status["code_answer"]}",
-                (0, 2)
-            ))
-            console.print(Padding(
+                f"Expected answer: {status["expected_code_answer"]}\n"
+                f"Your answer: {status["code_answer"]}\n"
                 f"stdout: {status["code_output"]}",
-                (1, 2)
+                (0, 2, 1, 2)
             ))
+
         elif status["status_msg"] == "Runtime Error":
             console.print(Padding("Runtime Error", (1, 2), style="bold red"))
-            console.print(Padding(status["runtime_error"], (0, 2)))
-            
+            console.print(Padding(status["runtime_error"], (0, 2, 1, 2)))
+        
+        elif status["status_msg"] == "Compile Error":
+            console.print(Padding("Compile Error", (1, 2), style="bold red"))
+            console.print(Padding(status["compile_error"], (0, 2, 1, 2)))            
 
     else:
         if status["status_msg"] == "Accepted":
@@ -65,43 +69,39 @@ def run(title_slug: str, lang: str, test: bool):
             console.print(Padding(
                 f"Runtime: {status["status_runtime"]} "
                 f"beats {round(status["runtime_percentile"], 2)}% of users "
-                f"using {status["pretty_lang"]}.",
-                (0, 2)
-            ))
-            console.print(Padding(
+                f"using {status["pretty_lang"]}.\n"
                 f"Memory: {status["status_memory"]} "
                 f"beats {round(status["memory_percentile"], 2)}% of users "
                 f"using {status["pretty_lang"]}.",
-                (0, 2)
+                (0, 2, 1, 2)
             ))
+
         else:
-            console.print(Padding(status["status_msg"], (1, 2), style="bold red"))
+            console.print(
+                Padding(status["status_msg"], (1, 2), style="bold red")
+            )
             if status["status_msg"] == "Wrong Answer":
                 console.print(Padding(
-                    f"Last testcase: {status["input_formatted"]}",
-                    (0, 2)
-                ))
-                console.print(Padding(
-                    f"Expected Answer: {status["expected_output"]}",
-                    (0, 2)
-                ))
-                console.print(Padding(
+                    f"Last testcase: {status["input_formatted"]}\n"
+                    f"Expected Answer: {status["expected_output"]}\n"
                     f"Your Answer: {status["code_output"]}",
-                    (0, 2)
+                    (0, 2, 1, 2)
                 ))
+
             elif status["status_msg"] == "Runtime Error":
-                console.print(Padding(status["full_runtime_error"], (0, 2)))
+                console.print(Padding(status["full_runtime_error"], (1, 2)))
+
             elif status["status_msg"] == "Compile Error":
-                console.print(Padding(status["full_compile_error"], (0, 2)))
+                console.print(Padding(status["full_compile_error"], (1, 2)))
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("titleslug")
-    parser.add_argument("lang")
-    parser.add_argument("test")
+    parser.add_argument("id", type=int)
+    parser.add_argument("lang", choices=EXT_MAP.keys())
+    parser.add_argument("test", type=bool)
     args = parser.parse_args()
 
-    run(args.titleslug, args.lang, ast.literal_eval(args.test))    
+    run(args.titleslug, args.lang, args.test)
