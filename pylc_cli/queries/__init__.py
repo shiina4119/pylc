28cc27cd42eb6420aa6fdd716c55e798ec27953f
shiina <152995083+shiina4119@users.noreply.github.com>
2024-06-30 19:58:10 +0530

    queries/__init__.py: add graphql transport object, remove generate_headers()

--- pylc_cli/queries/__init__.py
+++ pylc_cli/queries/__init__.py
@@ -1,14 +1,13 @@
+from gql.transport.aiohttp import AIOHTTPTransport
 from .. import cookies
-
 
 BASE_URL = "https://leetcode.com"
 GRAPHQL_URL = f"{BASE_URL}/graphql/"
+headers = {
+    "content-type": "application/json",
+    "Origin": BASE_URL,
+    "x-csrftoken": cookies["csrftoken"],
+    "Cookie": f"LEETCODE_SESSION={cookies['session']}; csrftoken={cookies['csrftoken']}",
+}
 
-
-def generate_headers() -> dict:
-    return {
-        "content-type": "application/json",
-        "Origin": BASE_URL,
-        "x-csrftoken": cookies["csrftoken"],
-        "Cookie": f"LEETCODE_SESSION={cookies['session']}; csrftoken={cookies['csrftoken']}",
-    }
+transport = AIOHTTPTransport(url=GRAPHQL_URL, headers=headers)
