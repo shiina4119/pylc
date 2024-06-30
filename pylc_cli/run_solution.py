67ca46617069c258ca9afcf71b5f4bea4102c639
shiina <152995083+shiina4119@users.noreply.github.com>
2024-06-30 22:53:30 +0530

    Rename run and solve functions

--- pylc_cli/run_solution.py
+++ pylc_cli/run_solution.py
@@ -16,7 +16,7 @@ def stringify_code(file_path: str) -> str:
     return "".join(lines)
 
 
-def run(id: int, lang: str, test: bool) -> None:
+def run_solution(id: int, lang: str, test: bool) -> None:
     res = dbcon.execute(f"SELECT id, title_slug FROM metadata WHERE frontend_id = {id}")
     data = res.fetchone()
     title_slug = data["title_slug"]
@@ -111,4 +111,4 @@ if __name__ == "__main__":
     parser.add_argument("test", type=bool)
     args = parser.parse_args()
 
-    run(args.titleslug, args.lang, args.test)
+    run_solution(args.titleslug, args.lang, args.test)
