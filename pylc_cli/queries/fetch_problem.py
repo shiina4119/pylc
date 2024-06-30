bffb5e8e2d467418b02b7bf48cc904c3e3a99c45
shiina <152995083+shiina4119@users.noreply.github.com>
2024-06-30 20:11:14 +0530

    queries/fetch_problem.py: change all functions to use gql client and async

--- pylc_cli/queries/fetch_problem.py
+++ pylc_cli/queries/fetch_problem.py
@@ -1,100 +1,83 @@
+from gql import Client, gql
 import requests
-from . import generate_headers, GRAPHQL_URL
+from . import transport
 
 
-def fetch_daily() -> int:
-    headers = generate_headers()
-    query = """
-    query {
-      activeDailyCodingChallengeQuestion {
-        question {
-          questionFrontendId
+async def fetch_daily() -> int:
+    async with Client(transport=transport) as session:
+        query = gql(
+            """
+            query {
+              activeDailyCodingChallengeQuestion {
+                question {
+                  questionFrontendId
+                }
+              }
+            }
+            """
+        )
+
+        result = await session.execute(document=query)
+        return int(
+            result["activeDailyCodingChallengeQuestion"]["question"][
+                "questionFrontendId"
+            ]
+        )
+
+
+async def fetch_problem_content(title_slug: str) -> str:
+    async with Client(transport=transport) as session:
+        query = gql(
+            """
+            query ($titleSlug: String!) {
+              question(titleSlug: $titleSlug) {
+                content
+              }
+            }
+            """
+        )
+        variables = {"titleSlug": title_slug}
+
+        result = await session.execute(document=query, variable_values=variables)
+        return result["question"]["content"]
+
+
+async def fetch_problem_snippets(title_slug: str) -> dict:
+    async with Client(transport=transport) as session:
+        query = gql(
+            """
+            query ($titleSlug: String!) {
+              question(titleSlug: $titleSlug) {
+                codeSnippets {
+                  langSlug
+                  code
+                }
+              }
+            }
+            """
+        )
+        variables = {"titleSlug": title_slug}
+
+        result = await session.execute(document=query, variable_values=variables)
+        snippets = {
+            snippet_map["langSlug"]: snippet_map["code"]
+            for snippet_map in result["question"]["codeSnippets"]
         }
-      }
-    }
-    """
-
-    response = requests.post(url=GRAPHQL_URL, json={"query": query}, headers=headers)
-    if response.status_code != 200:
-        # TODO: handle 403 errors nicely
-        raise ConnectionError
-
-    json = response.json()
-    return int(
-        json["data"]["activeDailyCodingChallengeQuestion"]["question"][
-            "questionFrontendId"
-        ]
-    )
+        return snippets
 
 
-def fetch_problem_content(title_slug: str) -> str:
-    headers = generate_headers()
-    query = """
-    query ($titleSlug: String!) {
-      question(titleSlug: $titleSlug) {
-        content
-      }
-    }
-    """
-    variables = {"titleSlug": title_slug}
-    response = requests.post(
-        url=GRAPHQL_URL, json={"query": query, "variables": variables}, headers=headers
-    )
-    if response.status_code != 200:
-        # TODO: handle 403 errors nicely
-        raise ConnectionError
+async def fetch_problem_testcases(title_slug: str) -> str:
+    async with Client(transport=transport) as session:
+        query = gql(
+            """
+            query ($titleSlug: String!) {
+              question(titleSlug: $titleSlug) {
+                exampleTestcaseList
+              }
+            }
+            """
+        )
+        variables = {"titleSlug": title_slug}
 
-    json = response.json()
-    return json["data"]["question"]["content"]
-
-
-def fetch_problem_snippets(title_slug: str) -> dict:
-    headers = generate_headers()
-    query = """
-    query ($titleSlug: String!) {
-      question(titleSlug: $titleSlug) {
-        codeSnippets {
-          langSlug
-          code
-        }
-      }
-    }
-    """
-    variables = {"titleSlug": title_slug}
-    response = requests.post(
-        url=GRAPHQL_URL, json={"query": query, "variables": variables}, headers=headers
-    )
-    if response.status_code != 200:
-        # TODO: handle 403 errors nicely
-        raise ConnectionError
-
-    json = response.json()
-
-    snippets = {
-        snippet_map["langSlug"]: snippet_map["code"]
-        for snippet_map in json["data"]["question"]["codeSnippets"]
-    }
-
-    return snippets
-
-
-def fetch_problem_testcases(title_slug: str) -> str:
-    headers = generate_headers()
-    query = """
-    query ($titleSlug: String!) {
-      question(titleSlug: $titleSlug) {
-        exampleTestcaseList
-      }
-    }
-    """
-    variables = {"titleSlug": title_slug}
-
-    response = requests.post(
-        url=GRAPHQL_URL, json={"query": query, "variables": variables}, headers=headers
-    )
-    if response.status_code != 200:
-        # TODO: handle 403 errors nicely
-        raise ConnectionError
-
-    json = response.json()
-    return json["data"]["question"]["exampleTestcaseList"]
+        result = await session.execute(document=query, variable_values=variables)
+        return result["question"]["exampleTestcaseList"]
